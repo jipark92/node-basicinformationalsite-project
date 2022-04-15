6e5318592d5f26801259603f8c9fe091fcee70a3
ji <jiyongprk@gmail.com>
2022-04-15 18:51:50 -0400

    add html elements and show homepage display

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
 const http = require('http')
+const fs = require('fs') 
+const url = require('url')
+
 const port = 3000
 
-const server = http.createServer(function(req, res){
-    res.write('Hello Nodessss')
-    res.end()
+const homeUrl = 'http://localhost:3000/'
+const aboutUrl = 'http://localhost:3000/about.html'
+
+console.log(aboutUrl)
+
+const server = http.createServer((req,res)=>{
+
+    if(req.url.pathname === '/about'){
+        fs.readFile('/home/norfkorean/node-basicinformationalsite-project/about.html', 'utf8' , (err,data)=>{
+            res.statusCode = 200
+            res.setHeader('Content-Type', 'text/html')
+            res.end(data)
+        })
+    } 
+    // else if(aboutUrl){
+    //     fs.readFile('/home/norfkorean/node-basicinformationalsite-project/about.html', 'utf8' , (err,data)=>{
+    //         res.statusCode = 200
+    //         res.setHeader('Content-Type', 'text/html')
+    //         res.end(data)
+    //     })
+    // }
+    
 })
 
-server.listen(port, function(error){
-    if (error){
-        console.log('404 page',error)
-    } else {
-        console.log("server is listening" + port)
-    }
+server.listen(port, () =>{
+    console.log(`server running at ${port}!!`)
 })
